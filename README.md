9478d70e92c0864852cac937613491ab6106ce23
Sean Reece <reece.sean@gmail.com>
2020-08-05 23:38:40 -0400

    main nav, improvements, readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # github-redesign-chrome-extention
 Opinionated tweaks to the github redesign in a fancy chrome extension
+
+<p align="center">
+  <img src="./images/logo.png" alt="Github++ logo"/>
+</p>
+
+### What?
+Github released a [redesign in June 2020](https://github.blog/changelog/2020-06-23-design-updates-to-repositories-and-github-ui/) that was mostly great, but there was a few things that I didn't like about it. Lets try to improve it!
+
+### Features
+
+- 👉Center the top nav 👈
+- 👉Center the main repo nav 👈
+- 🎉Add a top level tab for releases
+- TODO: Quick filter collapse pull request files
+- TODO: Customizable
+- TODO: Collapse files to bring README up
+
+
+#### BEFORE
+<p align="center">
+  <img src="./images/before.png" alt="Before Image"/>
+</p>
+
+#### AFTER
+<p align="center">
+  <img src="./images/after.png" alt="After Image"/>
+</p>
--- github.js
+++ github.js
@@ -1,65 +1,55 @@
-// function htmlToElement(html) {
-//     var template = document.createElement('template');
-//     html = html.trim(); // Never return a text node of whitespace as the result
-//     template.innerHTML = html;
-//     return template.content.firstChild;
-// }
 console.time('github++')
-const repoUrl = document.querySelector('meta[property~="og:url"').getAttribute('content')
-const selectedLink = document.querySelector('meta[name~="selected-link"').getAttribute('value')
+const repoUrl = document.querySelector('meta[property="og:url"').getAttribute('content')
+const selectedLink = document.querySelector('meta[name="selected-link"').getAttribute('value')
 const isReleasesSelected = selectedLink === 'repo_releases'
 const isCodeSelected = selectedLink === 'repo_source'
 
 let navTabs = document.querySelector('.UnderlineNav-body')
-let codeTab = navTabs.children[0]
+let codeTab = navTabs?.children[0]
 
-let releaseTab = codeTab.cloneNode(true)
-let releaseTabAnchor = releaseTab.firstElementChild
-let releaseTabSvg = releaseTabAnchor.firstElementChild
-let releaseTabName = releaseTabAnchor.children[1]
+if (codeTab) {
+    let releaseTab = codeTab.cloneNode(true)
+    let releaseTabAnchor = releaseTab.firstElementChild
+    let releaseTabSvg = releaseTabAnchor.firstElementChild
+    let releaseTabName = releaseTabAnchor.children[1]
 
-let releaseTabText = "Releases"
+    let releaseTabText = "Releases"
 
-// Get release count
-if (isCodeSelected) {
-    const releaseSidePanel = document.querySelector('a[href$="/releases"]')
-    releaseTabText = releaseSidePanel.innerHTML
-    chrome.storage.local.set({ releaseTabText }, function () {
-        console.log('Set Release Tab Text.');
-    });
-} else {
-    let uhh = chrome.storage.local.get('releaseTabText', function(data) {
-        releaseTabName.innerHTML = data.releaseTabText
-        console.log('get it', releaseTabText)
-    });
-    console.log('uhhh', uhh)
-}
+    // Get release count
+    if (isCodeSelected) {
+        const releaseSidePanel = document.querySelector('a[href$="/releases"]')
+        releaseTabText = releaseSidePanel.innerHTML
+        chrome.storage.local.set({ releaseTabText });
+    } else {
+        let uhh = chrome.storage.local.get('releaseTabText', function(data) {
+            releaseTabName.innerHTML = data.releaseTabText
+        });
+    }
 
-// Insert Releases tab
-navTabs.insertBefore(releaseTab, navTabs.children[1])
+    // Insert Releases tab
+    navTabs.insertBefore(releaseTab, navTabs.children[1])
 
-//Edit release tab 
-releaseTabSvg.firstElementChild.setAttribute('d', 'M2.5 7.775V2.75a.25.25 0 01.25-.25h5.025a.25.25 0 01.177.073l6.25 6.25a.25.25 0 010 .354l-5.025 5.025a.25.25 0 01-.354 0l-6.25-6.25a.25.25 0 01-.073-.177zm-1.5 0V2.75C1 1.784 1.784 1 2.75 1h5.025c.464 0 .91.184 1.238.513l6.25 6.25a1.75 1.75 0 010 2.474l-5.026 5.026a1.75 1.75 0 01-2.474 0l-6.25-6.25A1.75 1.75 0 011 7.775zM6 5a1 1 0 100 2 1 1 0 000-2z')
+    //Edit release tab 
+    releaseTabSvg.firstElementChild.setAttribute('d', 'M2.5 7.775V2.75a.25.25 0 01.25-.25h5.025a.25.25 0 01.177.073l6.25 6.25a.25.25 0 010 .354l-5.025 5.025a.25.25 0 01-.354 0l-6.25-6.25a.25.25 0 01-.073-.177zm-1.5 0V2.75C1 1.784 1.784 1 2.75 1h5.025c.464 0 .91.184 1.238.513l6.25 6.25a1.75 1.75 0 010 2.474l-5.026 5.026a1.75 1.75 0 01-2.474 0l-6.25-6.25A1.75 1.75 0 011 7.775zM6 5a1 1 0 100 2 1 1 0 000-2z')
 
-// Edit Name
-releaseTabName.setAttribute('data-content', 'Releases')
-releaseTabName.innerHTML = releaseTabText
+    // Edit Name
+    releaseTabName.setAttribute('data-content', 'Releases')
+    releaseTabName.innerHTML = releaseTabText
 
-// Edit link
-releaseTabAnchor.setAttribute('data-tab-item', 'releases-tab')
-releaseTabAnchor.setAttribute('href', `${repoUrl}/releases`)
-releaseTabAnchor.setAttribute('data-hotkey', 'g r')
-releaseTabAnchor.setAttribute('data-selected-links', 'repo_releases /SeanReece/pr-reporter-slack/releases')
+    // Edit link
+    releaseTabAnchor.setAttribute('data-tab-item', 'releases-tab')
+    releaseTabAnchor.setAttribute('href', `${repoUrl}/releases`)
+    releaseTabAnchor.setAttribute('data-hotkey', 'g r')
+    releaseTabAnchor.setAttribute('data-selected-links', 'repo_releases /SeanReece/pr-reporter-slack/releases')
 
-if (isReleasesSelected) {
-    const codeTabAnchor = codeTab.firstElementChild
-    codeTabAnchor.classList.remove('selected')
-    codeTabAnchor.removeAttribute('aria-current')
-    // releaseTabAnchor.classList.remove('selected')
-    // releaseTabAnchor.removeAttribute('aria-current')
-} else {
-    releaseTabAnchor.classList.remove('selected')
-    releaseTabAnchor.removeAttribute('aria-current')
+    if (isReleasesSelected) {
+        const codeTabAnchor = codeTab.firstElementChild
+        codeTabAnchor.classList.remove('selected')
+        codeTabAnchor.removeAttribute('aria-current')
+    } else {
+        releaseTabAnchor.classList.remove('selected')
+        releaseTabAnchor.removeAttribute('aria-current')
+    }
 }
 
 console.timeEnd('github++')
